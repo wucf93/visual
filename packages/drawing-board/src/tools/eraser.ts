e6a77cef18ee0c0aaffa93e533e488c9323475be
wuchengfan <wuchengfan@kuaishou.com>
2022-02-24 21:05:52 +0800

    fixd: 修复 linewidth 矩阵问题

--- packages/drawing-board/src/tools/eraser.ts
+++ packages/drawing-board/src/tools/eraser.ts
@@ -1,16 +1,20 @@
 import { ToolBase, ToolEvent } from "./base";
-import { LineElement } from '../elements/line'
+import { LineElement } from "../elements/line";
 
 export class EraserTool implements ToolBase {
-    onMouseDown({ renderer, pageX, pageY }: ToolEvent) {
-        const element = renderer.findElement(value => {
-            console.log(renderer.ctx.isPointInStroke(value.path2d, pageX, pageY))
-            return value instanceof LineElement && renderer.ctx.isPointInPath(value.path2d, pageX, pageY)
-        })
-        // console.log(element);
-        if (element) {
-            // renderer.exec("remove", element)
-            // renderer.render()
-        }
+  onClick({ renderer, pageX, pageY }: ToolEvent) {
+    const element = renderer.findElement((value) => {
+      if (!(value instanceof LineElement)) return false;
+      renderer.ctx.save();
+      value.lineWidth && (renderer.ctx.lineWidth = value.lineWidth);
+      const status = renderer.ctx.isPointInStroke(value.path2d, pageX, pageY);
+      renderer.ctx.restore();
+      return status;
+    });
+
+    if (element) {
+      renderer.exec("remove", element);
+      renderer.render();
     }
+  }
 }
